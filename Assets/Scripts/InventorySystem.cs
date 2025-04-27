5935e8ab2a25381a9b6a7f5dab7e082bf27bffce
Tehsin Shaik <tehsinshaikk@gmail.com>
2025-04-28 00:21:36 +0400

    Implement inventory system with object pickup and UI display
    - Pressing I key toggles the inventory UI.
    - When the player approaches an interactable object, its name is shown.
    - Clicking the object collects it and removes it from the scene.
    - Collected items are added to and displayed in the inventory UI.

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 // Manages the player's inventory UI screen toggle functionality
 public class InventorySystem : MonoBehaviour
@@ -11,6 +12,14 @@ public class InventorySystem : MonoBehaviour
 
     // Reference to the inventory UI panel in the scene
     public GameObject inventoryScreenUI;
+
+    public List<GameObject> slotList = new List<GameObject>();
+    public List<string> itemList = new List<string>();
+
+    private GameObject itemToAdd;
+    private GameObject whatSlotToEquip;
+
+    //public bool isFull;
 
     // Tracks whether the inventory screen is currently open
     public bool isOpen;
@@ -34,6 +43,19 @@ public class InventorySystem : MonoBehaviour
     void Start()
     {
         isOpen = false; // Inventory starts closed
+
+        PopulateSlotList();
+    }
+
+    private void PopulateSlotList()
+    {
+        foreach (Transform child in inventoryScreenUI.transform)
+        {
+            if (child.CompareTag("Slot"))
+            {
+                slotList.Add(child.gameObject);
+            }
+        }
     }
 
     // Listens for keypresses to open/close inventory
@@ -56,5 +78,46 @@ public class InventorySystem : MonoBehaviour
             Cursor.visible = false; // Hide cursor again
             isOpen = false;
         }
+    }
+
+    public bool checkIfFull()
+    {
+        int counter = 0;
+
+        foreach (GameObject slot in slotList)
+        {
+            if (slot.transform.childCount > 0)
+            {
+                counter += 1;
+            }
+        }
+
+        if (counter == 21)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void AddToInventory(String itemName)
+    {
+        whatSlotToEquip = FindNextEmptySlot();
+        itemToAdd = Instantiate(Resources.Load<GameObject>(itemName), whatSlotToEquip.transform.position, whatSlotToEquip.transform.rotation);
+        itemToAdd.transform.SetParent(whatSlotToEquip.transform);
+        itemList.Add(itemName);
+    }
+
+    private GameObject FindNextEmptySlot()
+    {
+        foreach (GameObject slot in slotList)
+        {
+            if (slot.transform.childCount == 0)
+            {
+                return slot;
+            }
+        }
+        return new GameObject();
     }
 }
